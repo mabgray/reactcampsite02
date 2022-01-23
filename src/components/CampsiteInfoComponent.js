c5d2be4f6f19b7a8c93bb556deedb5fa7f25185e
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-01-23 14:02:22 -0600

    presentational component Main

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -53,12 +53,13 @@ class CampsiteInfo extends Component {
 
         if(this.props.campsite){
             return (
-                 <div className="row">
-                     {this.renderCampsite(this.props.campsite)}
-                     {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
 
+                    </div>
                 </div>
-
         )
            
         } else {
