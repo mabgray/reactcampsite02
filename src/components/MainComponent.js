41773979f1c5994cbbf5098ed41ac48811914db5
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-01-29 10:30:44 -0600

    week 3 aboutus page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -52,6 +53,8 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' render={() => <About campsites={this.state.partners} />} />
+
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
