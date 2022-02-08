7a61519fb737e01489810e244d2859a6e3957200
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-02-08 14:30:30 -0600

    fetch and server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -14,6 +14,7 @@ import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 
@@ -114,7 +115,7 @@ function RenderCampsite({ campsite }) {
 	return (
 		<div className="col-md-5 m-1">
 			<Card>
-				<CardImg top src={campsite.image} alt={campsite.name} />
+				<CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
 				<CardBody>
 					<CardText>{campsite.description}</CardText>
 				</CardBody>
@@ -153,27 +154,27 @@ function RenderComments({ comments, addComment, campsiteId }) {
 }
 
 function CampsiteInfo(props) {
-    if (props.isLoading) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <Loading />
-                </div>
-            </div>
-        );
-    }
-    if (props.errMess) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h4>{props.errMess}</h4>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    if (props.campsite) {
+	if (props.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	}
+	if (props.errMess) {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<h4>{props.errMess}</h4>
+					</div>
+				</div>
+			</div>
+		);
+	}
+	if (props.campsite) {
 		return (
 			<div className="container">
 				<div className="row">
