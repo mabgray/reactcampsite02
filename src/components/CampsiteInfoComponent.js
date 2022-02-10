51c6026ac9ec9c3d373ca43ec2a7d51face5950c
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-02-10 14:20:01 -0600

    react transitions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -39,7 +39,7 @@ class CommentForm extends Component {
 
 	handleSubmit(values) {
 		this.toggleModal();
-		this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+		this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
 	}
 
 	render() {
@@ -124,7 +124,7 @@ function RenderCampsite({ campsite }) {
 	);
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
 	if (comments) {
 		return (
 			<div className="col-md-5 m-1">
@@ -145,7 +145,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
 					);
 				})}
 
-				<CommentForm campsiteId={campsiteId} addComment={addComment} />
+				<CommentForm campsiteId={campsiteId} postComment={postComment} />
 			</div>
 		);
 	} else {
@@ -193,7 +193,7 @@ function CampsiteInfo(props) {
 					<RenderCampsite campsite={props.campsite} />
 					<RenderComments
 						comments={props.comments}
-						addComment={props.addComment}
+						postComment={props.postComment}
 						campsiteId={props.campsite.id}
 					/>
 				</div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,7 +10,7 @@ import Home from './HomeComponent';
 import About from './AboutComponent'
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
@@ -23,7 +23,7 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {
-    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+    postComment: (campsiteId, rating, author, text) => (PostComment(campsiteId, rating, author, text)),
     fetchCampsites: () => (fetchCampsites()),
     resetFeedbackForm: () => (actions.reset('feedbackForm')),
     fetchComments: () => (fetchComments()),
@@ -64,22 +64,25 @@ class Main extends Component {
                 errMess={this.props.campsites.errMess}
                 comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                 commentsErrMess={this.props.comments.errMess}
-                addComment={this.props.addComment}
+                postComment={this.props.postComment}
             />      
             );
         };
         return (
             <div>
-                <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
-                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-                    <Route exact path='/aboutus' render={() => <About campsites={this.props.partners} />} />
-
-                    <Redirect to='/home' />
-                </Switch>
+               <Header />
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                        <Switch>
+                            <Route path='/home' component={HomePage} />
+                            <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
+                            <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                           <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
+                            <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
+                            <Redirect to='/home' />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
                 <Footer />
             </div>
         );
